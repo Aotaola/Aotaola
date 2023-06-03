ae9cfb10483dc2b759b28ecb1ad572be5d1a9435
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-06-03 18:37:30 -0400

    router fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import Home from './components/Home.jsx';
 import Navbar from './components/Navbar.jsx';
-import Resume from './components/Resume.jsx'; 
-import Blog from './components/Blog.jsx'; 
-import Artwork from './components/Artwork.jsx';
 import Contact from './components/Contact';
 import { Route, Routes } from "react-router-dom";
 
@@ -17,9 +14,6 @@ function App() {
       <div Classname = "Nav-container">
         <Routes>
           <Route path = "/" element = {<Home/>} Classname = "Home_Route"/>
-          <Route path = "/Resume" element = {<Resume/>}/>
-          <Route path = "/Blog" element = {<Blog/>}/>
-          <Route path = "/Artwork" element = {<Artwork/>}/>
           <Route path = "/Contact" element = {<Contact/>}/>
         </Routes>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Otaola_Alejandro_Resume_FL from "./files/Otaola_Alejandro_Resume_FL.pdf"
+import Otaola_Alejandro_Resume_FL from "./files/Otaola_Alejandro_Resume_FL.pdf";
 
 
 const Navbar = () => {
     return ( 
         <div className="navcontainer">
-        <div className="Navbar">
-            <nav>
-           <button className="nav_buttn">
-            <Link to="/">Home</Link> 
-            </button> 
-            <br/>
-            <button className="nav_buttn">
-               < Link to = "/Contact">Contact</Link>
-            </button>
-            <br/>
-            </nav>
+             <div className="Navbar">
+                 <nav>
+                     <button className="nav_buttn">
+                         <Link to="/">Home</Link> 
+                    </button> 
+                             <br/>
+                     <button className="nav_buttn">
+                         < Link to = "/Contact">Contact</Link>
+                      </button>
+                             <br/>
+                 </nav>
 
-         </div>
+                 </div>
         
-         <div className="cv_container">
+             <div className="cv_container">
 
-            <button className="cv_buttn">
-                <a href = {Otaola_Alejandro_Resume_FL}>Resume</a>
-            </button> 
-         </div>
+                    <button className="cv_buttn">
+                      <a href={Otaola_Alejandro_Resume_FL}>Resume</a>
+                    </button> 
+             </div>
          
         </div>
      );
