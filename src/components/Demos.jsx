864c352c04a48404b5871fa31a54ac8d9e60da6e
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-07-31 19:46:49 -0400

    sapiam demo

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import NightlifeApp from './vids/NighlifeApp.mp4';
+import sapiam_Demo from './vids/sapiam_Demo.mp4';
 
 
 
 const Demos = () => {
 
     return ( 
+        <>
         <div className="demos-container">
+            <br/>
             <h1>Project Demos</h1>
             <div className="demos-content">
                 <div className="player-wrapper">
+                    <ReactPlayer url = {sapiam_Demo}
+                        controls   
+                        playbackRate={1.25}/>
+                </div>
+                <br/>
+                <div className="player-wrapper">
                     <ReactPlayer url = {NightlifeApp}
                         controls   
-                        playbackRate={1.25}                 
-                    />
+                        playbackRate={1.25}/>
                 </div>
             </div>
+            <br/>
         </div>
+        </>
+        
      );
 }
  
