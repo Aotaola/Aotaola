4d333c0d2b983f9b388bb48acc2b026d3ca4e078
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-08-02 17:00:41 -0400

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Otaola_Alejandro_Resume_NY from "./files/Otaola_Alejandro_Resume_NY.pdf";
+import Alejandro_Otaola_Resume_FL from "./files/Alejandro_Otaola_Resume_FL.pdf";
 
 
 const Navbar = () => {
@@ -27,7 +27,7 @@ const Navbar = () => {
              <div className="cv_container">
 
                     <button className="cv_buttn">
-                      <a href={Otaola_Alejandro_Resume_NY} target = "_blank" rel="noopener noreferrer">Resume</a>
+                      <a href={Alejandro_Otaola_Resume_FL} target = "_blank" rel="noopener noreferrer">Resume</a>
                     </button> 
              </div>
          
