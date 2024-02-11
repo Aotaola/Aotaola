dbc25af05222c56f780e970896cc0f9db0554399
Aotaola <115048502+Aotaola@users.noreply.github.com>
2024-02-10 22:18:24 -0500

    video resize

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -16,8 +16,8 @@ const Demos = () => {
                 <br/>
                     <div className="player-wrapper">
                     <iframe title="My Unique Video Title"
-                    width="560"
-                    height="315"
+                    width='95%'
+                    height='95%'
                      src="https://www.loom.com/embed/f4daf59699c6421aaf5bc8b255c3c90d?sid=05e82a5b-203a-40e7-a48d-2e34dc5e5a62" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
                     </iframe>
                     </div>
@@ -29,8 +29,8 @@ const Demos = () => {
                     <div className="player-wrapper">
                     <iframe 
                      title="Unique Video Title"
-                     width="560"
-                     height="315"
+                     width='95%'
+                     height='95%'
                     src="https://www.loom.com/embed/edcbe895c8ce499ba8059f71d8095ade?sid=b93dd829-51db-4dd7-996c-0c0575bdaef0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen >
                     </iframe>
                     </div>
